5c3cf808ebc0295c386fffb98e9919b5a94eb741
Joel Fåk <joel.fak@niradynamics.se>
2019-12-26 22:53:22 +0100

    Day 10 part 2

--- joelfak-python/day10.py
+++ joelfak-python/day10.py
@@ -4,6 +4,7 @@ from helpfunctions import *
 import unittest, sys, math
 from collections import defaultdict
 from pprint import pprint
+from functools import reduce
 
 class Point:
     def __init__(self, x, y):
@@ -39,6 +40,12 @@ class Vector(Point):
     def getAngle(self):
         return math.atan2(self.y, self.x)
 
+    def getClockwiseAngleFromNegativeY(self):
+        angle = self.getAngle() + math.pi/2
+        if angle < 0:
+            angle += 2*math.pi
+        return angle
+
     def getMinimalVector(self):
         gcd = math.gcd(self.x, self.y)
         if gcd == 0:
@@ -53,32 +60,76 @@ def getSetOfAsteroids(asteroidMap):
                 visibleAsteroids.add(Point(x,y))
     return visibleAsteroids
 
-def getNumberOfVisibleAsteroids(asteroidMap, base):
+def calculateVisibleAsteroids(asteroidMap, base, verbose=False):
     d = defaultdict(list)
-    for y in range(len(asteroidMap)):
-        for x in range(len(asteroidMap[y])):
-            if asteroidMap[y][x] == '#':
-                asteroid = Point(x, y)
-                vectorToAsteroid = Vector.fromObject(asteroid - base)
-                d[vectorToAsteroid.getMinimalVector()].append(asteroid)
-    # pprint(d)
-    return len(d) - 1
+    for asteroid in getSetOfAsteroids(asteroidMap):
+        if asteroid != base:
+            vectorToAsteroid = Vector.fromObject(asteroid - base)
+            d[vectorToAsteroid.getMinimalVector()].append(vectorToAsteroid)
 
-def findBestBaseLocation(asteroidMap):
+    for direction, vectors in d.items():
+        vectors.sort(key=lambda vector: vector.getLength())
+
+    if verbose:
+        pprint(d)
+
+    return d
+
+def getNumberOfVisibleAsteroids(asteroidMap, base, verbose=False):
+    d = calculateVisibleAsteroids(asteroidMap, base, verbose)
+    return len(d)
+
+def findBestBaseLocation(asteroidMap, verbose=False):
     asteroids = getSetOfAsteroids(asteroidMap)
     p = max(asteroids, key = lambda x: getNumberOfVisibleAsteroids(asteroidMap, x))
-    return (p, getNumberOfVisibleAsteroids(asteroidMap, p))
+    return (p, getNumberOfVisibleAsteroids(asteroidMap, p, verbose=verbose))
+
+def findBaseInMap(asteroidMap):
+    for y in range(len(asteroidMap)):
+        for x in range(len(asteroidMap[y])):
+            if asteroidMap[y][x] == 'X':
+                return Point(x, y)
+    return None
+
+def destroyAsteroids(asteroidMap, numAsteroidsToDestroy=math.inf, findBestBase=False, verbose=False):
+    if findBestBase:
+        base = findBestBaseLocation(asteroidMap, verbose=verbose)[0]
+    else:
+        base = findBaseInMap(asteroidMap)
+
+    directionsMap = calculateVisibleAsteroids(asteroidMap, base, verbose)
+    sortedDirections = sorted(directionsMap.keys(), key=lambda vector: vector.getClockwiseAngleFromNegativeY())
+
+    numberOfDestroyedAsteroids = 0
+    latestDestroyedAsteroid = None
+    done = False
+    while sum([len(vector) for vector in directionsMap.values()]) > 0:
+        for direction in sortedDirections:
+            if len(directionsMap[direction]) > 0:
+                latestDestroyedAsteroid = base + directionsMap[direction].pop(0)
+                numberOfDestroyedAsteroids += 1
+                if numberOfDestroyedAsteroids >= numAsteroidsToDestroy:
+                    done = True
+                    break
+        if done:
+            break
+
+    if verbose:
+        print("NumberOfDestroyedAsteroids: {}".format(numberOfDestroyedAsteroids))
+    return latestDestroyedAsteroid
 
 def part1(asteroidMap):
     asteroidMap = [[char for char in row] for row in asteroidMap]
     return findBestBaseLocation(asteroidMap)
 
-def part2(data):
-    return 0
+def part2(asteroidMap):
+    asteroidMap = [[char for char in row] for row in asteroidMap]
+    asteroid = destroyAsteroids(asteroidMap, 200, findBestBase=True)
+    return asteroid.x*100+asteroid.y
 
 ## Unit tests ########################################################
 
-class TestDay10(unittest.TestCase):
+class TestDay10_part1(unittest.TestCase):
     asteroidMap1 = [ '.#..#',
                     '.....',
                     '#####',
@@ -191,11 +242,91 @@ class TestDay10(unittest.TestCase):
                         '#.#.#.#####.####.###',
                         '###.##.####.##.#..##' ]
         asteroidMap = [[char for char in row] for row in asteroidMap]
-        self.assertEqual(findBestBaseLocation(asteroidMap), (Point(11,13), 210))
+        self.assertEqual(findBestBaseLocation(asteroidMap, verbose=False), (Point(11,13), 210))
+
+class TestDay10_part2_map1(unittest.TestCase):
+    asteroidMap1 = [ '.#....#####...#..',
+                    '##...##.#####..##',
+                    '##...#...#.#####.',
+                    '..#.....X...###..',
+                    '..#.#.....#....##' ]
+    asteroidMap1 = [[char for char in row] for row in asteroidMap1]
+
+    def test_findBaseInMap(self):
+        self.assertEqual(findBaseInMap(self.asteroidMap1), Point(8,3))
+
+    def test_destroyAsteroids1(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, 4), Point(10, 0))
+
+    def test_destroyAsteroids2(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, 7), Point(12, 1))
+
+    def test_destroyAsteroids3(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, 36), Point(14, 3))
+
+class TestDay10_part2_map2(unittest.TestCase):
+    asteroidMap1 = [   '.#..##.###...#######',
+                        '##.############..##.',
+                        '.#.######.########.#',
+                        '.###.#######.####.#.',
+                        '#####.##.#.##.###.##',
+                        '..#####..#.#########',
+                        '####################',
+                        '#.####....###.#.#.##',
+                        '##.#################',
+                        '#####.##.###..####..',
+                        '..######..##.#######',
+                        '####.##.####...##..#',
+                        '.#####..#.######.###',
+                        '##...#.##########...',
+                        '#.##########.#######',
+                        '.####.#.###.###.#.##',
+                        '....##.##.###..#####',
+                        '.#.#.###########.###',
+                        '#.#.#.#####.####.###',
+                        '###.##.####.##.#..##' ]
+    asteroidMap1 = [[char for char in row] for row in asteroidMap1]
+
+    def test_destroyAsteroids1(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, 1, findBestBase=True), Point(11,12))
+
+    def test_destroyAsteroids2(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, 2, findBestBase=True), Point(12,1))
+
+    def test_destroyAsteroids3(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, 3, findBestBase=True), Point(12,2))
+
+    def test_destroyAsteroids4(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, 10, findBestBase=True), Point(12,8))
+
+    def test_destroyAsteroids5(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, 20, findBestBase=True), Point(16,0))
+
+    def test_destroyAsteroids6(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, 50, findBestBase=True), Point(16,9))
+
+    def test_destroyAsteroids7(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, 100, findBestBase=True), Point(10,16))
+
+    def test_destroyAsteroids8(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, 199, findBestBase=True), Point(9,6))
+
+    def test_destroyAsteroids9(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, 200, findBestBase=True), Point(8,2))
+
+    def test_destroyAsteroids10(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, 201, findBestBase=True), Point(10,9))
+
+    def test_destroyAsteroids11(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, 299, findBestBase=True), Point(11,1))
+
+    def test_destroyAsteroids12(self):
+        self.assertEqual(destroyAsteroids(self.asteroidMap1, findBestBase=True), Point(11,1))
+
 ## Main ########################################################
 
 if __name__ == '__main__':
 
     print("Advent of code day 10")
     print("Part1 result: {}".format(part1(getStringsFromFile(sys.argv[1]))))
-    # print("Part2 result: {}".format(part2(getIntsFromFile(sys.argv[1]))))
+    print("Part2 result: {}".format(part2(getStringsFromFile(sys.argv[1]))))
